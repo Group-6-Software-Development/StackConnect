23e139c22ee137f854560a2298856498b39627b9
Sami <sami.mattila3@metropolia.fi>
2023-11-20 01:46:58 +0200

    JobsPage.css updated, bNavbar.js updated, JobsPage.js updated. Search field has been implemented with JobsPage.

--- sprint_2/stack-connect/src/components/bNavbar.js
+++ sprint_2/stack-connect/src/components/bNavbar.js
@@ -1,12 +1,25 @@
 // bNavbar.js
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/Navbar.css";
 import logo from "../images/logo2.svg";
 import { Button, Container, Nav, Navbar, Row, Col, Form } from "react-bootstrap";
 
-function bNavbar() {
+function BNavbar() {
+  const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    // Redirect to JobsPage with the search query as a parameter
+    navigate(`/jobs?search=${encodeURIComponent(searchQuery)}`);
+  };
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="myBlue" variant="dark">
       <Container>
@@ -16,10 +29,16 @@ function bNavbar() {
 
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Form inline className="mx-auto">
+          <Form inline className="mx-auto" onSubmit={handleSearchSubmit}>
             <Row>
               <Col xs="auto">
-                <Form.Control type="text" placeholder="Search..." className="mr-sm-2" inline={1} />
+                <Form.Control
+                  type="text"
+                  placeholder="Search..."
+                  className="mr-sm-2"
+                  value={searchQuery}
+                  onChange={handleSearchChange}
+                />
               </Col>
               <Col xs="auto">
                 <Button type="submit">Submit</Button>
@@ -27,8 +46,12 @@ function bNavbar() {
             </Row>
           </Form>
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/jobs">Jobs</Nav.Link>
-            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/jobs">
+              Jobs
+            </Nav.Link>
+            <Nav.Link as={Link} to="/login">
+              Login
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -36,4 +59,4 @@ function bNavbar() {
   );
 }
 
-export default bNavbar;
+export default BNavbar;
