bd8366d86d26240c1270cfeb991fdf87ee65eb95
Kim Löfgren <lofgrenkim@outlook.com>
2023-11-20 14:56:01 +0200

    Add Footer component to App.js

--- sprint_2/stack-connect/src/App.js
+++ sprint_2/stack-connect/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/bNavbar";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
+      <Footer />
     </div>
   );
 }
