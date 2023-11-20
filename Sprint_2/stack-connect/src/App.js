243981fdc90b1c84578dde9b836dd6f7d6275257
MakeLord95 <juhani.markus@gmail.com>
2023-11-20 16:37:05 +0200

    -

--- Sprint_2/stack-connect/src/App.js
+++ Sprint_2/stack-connect/src/App.js
@@ -1,21 +1,13 @@
 import "./App.css";
 import Navbar from "./components/bNavbar";
 import Hero from "./components/Hero";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import EmployerProfile from "./components/EmployerProfile";
-import DeveloperProfile from "./components/DeveloperProfile";
-
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/employer-profile" element={<EmployerProfile />} />
-          <Route path="/developer-profile" element={<DeveloperProfile />} />
-        </Routes>
-      </BrowserRouter>
+      <Navbar />
+      <Hero />
+      <Footer />
     </div>
   );
 }
