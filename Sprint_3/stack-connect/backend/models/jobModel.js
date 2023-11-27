2527c824730b994933460ba33aa33e05b85081fa
Kim Löfgren <lofgrenkim@outlook.com>
2023-11-27 07:10:54 +0200

    fe folder, backend for job posts

---
+++ Sprint_3/stack-connect/backend/models/jobModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const jobSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    requirements: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    skills: [
+      {
+        name: String,
+        level: Number,
+      },
+    ],
+    user_id: {
+      type: String,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Job", jobSchema);
